685b51a51ef8a17ea45f196eaf6e06c0b61d4b74
jg485 <jg485host@gmail.com>
2013-06-02 12:54:36 +0100

    Added tests for validation of first_name, last_name, profile_name. Added a fixture.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,36 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "user has a first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+  
+  test "user has a last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "user has a profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "user has a unique profile name" do
+  	user= User.new
+  	user.profile_name = "jasonseifer"
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "user should have a profile name without spaces" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
 end
